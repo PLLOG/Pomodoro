dc3ddf2673d7a0199e84eba6675a9af7b9ff5c17
Oleg Raevskikh <plog475@gmail.com>
2025-12-24 19:57:17 +0500

    added functions to list your goals and timers, assembled the project into fuctioning script

--- main.py
+++ main.py
@@ -2,36 +2,65 @@ import winsound
 import time
 import csv
 import os
-def add_goal(fileName,goalName,pomNum):
+def list_goals(fileName):
+    with open(fileName,'r',encoding='utf-8',newline='') as csvfile:
+        reader=csv.reader(csvfile)
+        next(reader,None)
+        for row in reader:
+            if not row:
+                print('Конец списка')
+            else:
+                print (f'В цели {row[0]}, у вас {row[1]} помидорок')
+def list_timers(fileName):
+    with open(fileName,'r',encoding='utf-8',newline='') as csvfile:
+        reader=csv.reader(csvfile)
+        for row in reader:
+            match row[0]:
+                case 'session':
+                    print(f'Длительность сессии концетрации :{row[1]} минут')
+                case 's_break':
+                    print(f'Длительность маленького перерыва :{row[1]} минут')
+                case 'b_break':
+                    print(f'Длительность большого перерыва :{row[1]} минут')
+                case _:
+                    pass
+            
+def add_goal(fileName,goalName):
     with open (fileName,'a',encoding='utf-8',newline='')as csvfile:
         filewriter = csv.writer(csvfile,delimiter=',')
-        filewriter.writerow([goalName,0,pomNum])
-def delete_goal(fileName,goalName,column_name=0):
-    temp_file = fileName +'.tmp'
-    with open(fileName,'r',newline='',encoding='utf-8') as infile,\
-    open (temp_file,'w',newline='',encoding='utf-8') as outfile:
-
-        reader=csv.reader(infile)
-        writer=csv.writer(outfile)
-
-        header = next(reader)
-        writer.writerow(header)
-
-        try:
-            column_index = header.index(column_name)
-        except ValueError:
-            print(f'Error: Column {column_name} not found.')
-            return
-
+        filewriter.writerow([goalName,0])
+def delete_goal(fileName,goalName):
+    with open(fileName,'r',newline='',encoding='utf-8')as csvfile:
+        reader=csv.reader(csvfile)
+        list_of_goals=[]
+        next(reader)
         for row in reader:
-            if row[column_index] != goalName:
-                writer.writerow(row)
+            if not row:
+                print('Список пуст')
+                return
+            else:
+                list_of_goals.append(row[0])
+    if goalName in list_of_goals:
+        temp_file = fileName +'.tmp'
+        with open(fileName,'r',newline='',encoding='utf-8') as infile,\
+        open (temp_file,'w',newline='',encoding='utf-8') as outfile:
 
-    os.replace(temp_file,fileName)
-    print('deleted')
+            reader=csv.reader(infile)
+            writer=csv.writer(outfile)
+
+            header = next(reader)
+            writer.writerow(header)
+            for row in reader:
+                if row[0] != goalName:
+                    writer.writerow(row)
+
+        os.replace(temp_file,fileName)
+        print('Цель удалена')
+    else:
+        print('Цель не найдена')
 def add_pom_to_goal(fileName,goalName):
     rows = []
-    with open(fileName, mode='r', newline='', encoding='utf-8') as infile:
+    with open(fileName,'r', newline='', encoding='utf-8') as infile:
         reader = csv.reader(infile)
         header = next(reader) 
         rows.append(header)
@@ -45,10 +74,41 @@ def add_pom_to_goal(fileName,goalName):
             break
     
     
-    with open(fileName, mode='w', newline='', encoding='utf-8') as outfile:
+    with open(fileName,'w', newline='', encoding='utf-8') as outfile:
         writer = csv.writer(outfile)
         writer.writerows(rows)
-    print(f"File '{fileName}' has been updated.")
+    
+
+def timer_set(fileName,timerName,newTime):
+    rows = []
+    with open(fileName,'r', newline='', encoding='utf-8') as infile:
+        reader = csv.reader(infile)
+        header = next(reader) 
+        rows.append(header)
+        for row in reader:
+            rows.append(row)
+            
+    for i, row in enumerate(rows[0:], start=0):
+        if len(row) > 1 and row[0] == timerName: 
+            row[1] = newTime
+            break
+    
+    
+    with open(fileName,'w', newline='', encoding='utf-8') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerows(rows)
+    
+def get_time(fileName,timerName):
+    with open(fileName,'r',newline='',encoding='utf-8')as csvfile:
+        reader=csv.reader(csvfile)
+        for row in reader:
+            if row[0] == timerName:
+                return float(row[1])
+def get_total_pom(fileName):
+    with open(fileName,'r',encoding='utf-8',newline='') as csvfile:
+        reader=csv.reader(csvfile)
+        for row in reader:
+            return row[3]
 def play_sound(name):
     WAV_FILE = name
     PLAY_FLAGS = winsound.SND_FILENAME | winsound.SND_LOOP | winsound.SND_ASYNC
@@ -59,6 +119,7 @@ def session(sessionTimeInput):
     timerStart=time.time()
     sessionTimeSec=float(sessionTimeInput*60)
     timerFinish=timerStart+sessionTimeSec
+    print('Сессия началась!')
     play_sound('sound\\clock-ticking.wav')
     while True:
         time.sleep(1)
@@ -69,8 +130,9 @@ def session(sessionTimeInput):
             stop_all_sound()
             break
     
-    print ('done')
+    print ('Сессия завершена!')
 def s_break(breakTimeInput):
+    print(f'Время перерыва! Отдохните {breakTimeInput} минут')
     timerStart = time.time()
     breakTime=float(breakTimeInput*60)
     timerFinish=timerStart+breakTime
@@ -80,6 +142,106 @@ def s_break(breakTimeInput):
             play_sound('sound\\alarm.wav')
             time.sleep(3)
             stop_all_sound()
+            print('Перерыв завершен!')
             break
-        
+def session_progress():
+    sessionNum=0
+    while True:
+        sessionName=input('Введите название цели, для выхода "выход": ')
+        if sessionName == 'выход':
+            break
+        else:
+            with open('goal_list.csv','r',encoding='utf-8',newline='')as csvfile:
+                reader=csv.reader(csvfile)
+                list_of_goals=[]
+                next(reader,None)
+                for row in reader:
+                    if not row:
+                        pass
+                    else:
+                        list_of_goals.append(row[0])
+            if sessionName in list_of_goals:
+                choose_to_cont=input('''Такая цель сессии уже существует
+                                        Хотите продолжить работать над ней?(да/нет)
+                                        :''')
+                match choose_to_cont:
+                    case 'да':
+                        session(get_time('time_set.csv','session'))
+                        add_pom_to_goal('goal_list.csv',sessionName)
+                        sessionNum+=1
+                        if sessionNum>4:
+                            s_break(get_time('time_set.csv','b_break'))
+                        else:
+                            s_break(get_time('time_set.csv','s_break'))
+                    case 'нет':
+                            continue
+            else:
+                
+                add_goal('goal_list.csv',sessionName)
+                session(get_time('time_set.csv','session'))
+                add_pom_to_goal('goal_list.csv',sessionName)
+                sessionNum+=1
+                if sessionNum>4:
+                    s_break(get_time('time_set.csv','b_break'))
+                else:
+                    s_break(get_time('time_set.csv','s_break'))
+while True:
+    print(f'Вы заработали {get_total_pom("goal_list.csv")} помидорок')
+    choice = input('''
+    Для начала сессии введите "старт"
+    Для списка целей введите "список"
+    Для удаления цели введите "удалить"
+    Для настройки таймеров введите "настройка"
+    Для выхода введите "выход"
+    :''')
 
+    match choice:
+        case 'старт':
+            session_progress()
+        case 'список':
+            list_goals('goal_list.csv')
+        case 'удалить':
+            list_goals('goal_list.csv')
+            choose_to_del=input('Введите цель для удаления: ')
+            delete_goal('goal_list.csv',choose_to_del)
+            
+        case 'настройка':
+            list_timers('time_set.csv')
+            choose_time_to_cg=input(f'''
+    Введите название таймера для изменения
+    (большой/маленький/сессия):''')
+            match choose_time_to_cg:
+                case 'большой':
+                    time_inp=float(input('Введите новое время в минутах:'))
+                    timer_set('time_set.csv','b_break',time_inp)
+                case 'маленький':
+                    time_inp=float(input('Введите новое время в минутах:'))
+                    timer_set('time_set.csv','s_break',time_inp)
+                case 'сессия':
+                    time_inp=float(input('Введите новое время в минутах:'))
+                    timer_set('time_set.csv','session',time_inp)
+                case _:
+                    print('Неверное имя таймера')
+            
+        case 'выход':
+            break
+
+        case _:
+            print ('нераспознанная команда')
+            
+                            
+                
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            
